fb11004dad12b6edb1ea6a70e4374bd2b2041985
Max Boyko <maxboykomax@gmail.com>
2020-11-23 16:37:00 -0800

    refactor: Load app settings dynamically

--- project/__init__.py
+++ project/__init__.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, jsonify
 from flask_restx import Resource, Api
 
@@ -6,14 +7,13 @@ app = Flask(__name__)
 
 api = Api(app)
 
-app.config.from_object('project.config.DevelopmentConfig')
+app_settings = os.getenv("APP_SETTINGS")
+app.config.from_object(app_settings)
 
 
 class Ping(Resource):
     def get(self):
-        return {
-            'status': 'success',
-            'message': 'pong!'
-        }
+        return {"status": "success", "message": "pong!"}
 
-api.add_resource(Ping, '/ping')
+
+api.add_resource(Ping, "/ping")
