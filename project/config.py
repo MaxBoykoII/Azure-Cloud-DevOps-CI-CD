7361bec8e6d9e5df297c4325c9a9c0d1227a5710
Max Boyko <maxboykomax@gmail.com>
2020-11-23 16:51:25 -0800

    test: Add unit tests for the app config

--- project/config.py
+++ project/config.py
@@ -1,11 +1,15 @@
 class BaseConfig:
     TESTING = False
+    SECRET_KEY = "SECRET"
+
 
 class DevelopmentConfig(BaseConfig):
     pass
 
+
 class TestingConfig(BaseConfig):
     TESTING = True
+
 
 class ProductionConfig(BaseConfig):
     pass
---
+++ project/tests/unit/test_config.py
@@ -0,0 +1,19 @@
+import os
+
+
+def test_development_config(test_app):
+    test_app.config.from_object("project.config.DevelopmentConfig")
+    assert test_app.config["SECRET_KEY"] == "SECRET"
+    assert not test_app.config["TESTING"]
+
+
+def test_testing_config(test_app):
+    test_app.config.from_object("project.config.TestingConfig")
+    assert test_app.config["SECRET_KEY"] == "SECRET"
+    assert test_app.config["TESTING"]
+
+
+def test_production_config(test_app):
+    test_app.config.from_object("project.config.ProductionConfig")
+    assert test_app.config["SECRET_KEY"] == "SECRET"
+    assert not test_app.config["TESTING"]
