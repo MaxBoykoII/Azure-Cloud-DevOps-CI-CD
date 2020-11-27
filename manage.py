311e6fcb1e09c7665edda08de64cdf1160290617
Max Boyko <maxboykomax@gmail.com>
2020-11-26 18:06:21 -0800

    refactor: Apply the application factory pattern

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 from flask.cli import FlaskGroup
 
-from src import app
+from src import create_app
 
-cli = FlaskGroup(app)
+cli = FlaskGroup(create_app=create_app)
 
 if __name__ == "__main__":
     cli()
--- src/__init__.py
+++ src/__init__.py
@@ -1,19 +1,21 @@
 import os
 
 from flask import Flask
-from flask_restx import Api, Resource
-
-app = Flask(__name__)
-
-api = Api(app)
-
-app_settings = os.getenv("APP_SETTINGS")
-app.config.from_object(app_settings)
 
 
-class Ping(Resource):
-    def get(self):
-        return {"status": "success", "message": "pong!"}
+def create_app(script_info=None):
 
+    app = Flask(__name__)
 
-api.add_resource(Ping, "/ping")
+    app_settings = os.getenv("APP_SETTINGS")
+    app.config.from_object(app_settings)
+
+    from src.api.ping import ping_blueprint
+
+    app.register_blueprint(ping_blueprint)
+
+    @app.shell_context_processor
+    def ctx():
+        return {"app": app}
+
+    return app
