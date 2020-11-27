331d179639fa33a77ac458830d5ff24f211e18b0
Max Boyko <maxboykomax@gmail.com>
2020-11-26 18:33:42 -0800

    refactor: Group methods under an AppFactory class

--- src/__init__.py
+++ src/__init__.py
@@ -4,22 +4,35 @@ from flask import Flask
 
 
 def create_app(script_info=None):
-
     app = Flask(__name__)
+    factory = AppFactory(app)
 
-    set_config(app)
-
-    from src.api.ping import ping_blueprint
-
-    app.register_blueprint(ping_blueprint)
-
-    @app.shell_context_processor
-    def ctx():
-        return {"app": app}
+    factory.configure()
 
     return app
 
 
-def set_config(app):
-    app_settings = os.getenv("APP_SETTINGS")
-    app.config.from_object(app_settings)
+class AppFactory:
+    def __init__(self, app):
+        self.app = app
+
+    def configure(self):
+        self.set_config()
+        self.set_blueprints()
+        self.set_ctx()
+
+    def set_config(self):
+        app_settings = os.getenv("APP_SETTINGS")
+        self.app.config.from_object(app_settings)
+
+    def set_blueprints(self):
+        from src.api.ping import ping_blueprint
+
+        self.app.register_blueprint(ping_blueprint)
+
+    def set_ctx(self):
+        app = self.app
+
+        @app.shell_context_processor
+        def ctx():
+            return {"app": app}
