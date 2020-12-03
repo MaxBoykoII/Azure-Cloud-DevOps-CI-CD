dbce325b516d1390612238406171a2185a78f8ce
Max Boyko <maxboykomax@gmail.com>
2020-12-02 23:22:17 -0800

    test: Update prediction test and add test for invalid json keys

--- src/tests/functional/test_predict.py
+++ src/tests/functional/test_predict.py
@@ -10,12 +10,12 @@ def test_predict(test_app):
         "/predict",
         data=json.dumps(
             {
-                "CHAS": {"0": 0},
-                "RM": {"0": 6.575},
-                "TAX": {"0": 296.0},
-                "PTRATIO": {"0": 15.3},
-                "B": {"0": 396.9},
-                "LSTAT": {"0": 4.98},
+                "CHAS": {"0": 0, "1": 0},
+                "RM": {"0": 6.575, "1": 5.5},
+                "TAX": {"0": 296.0, "1": 295.0},
+                "PTRATIO": {"0": 15.3, "1": 243.0},
+                "B": {"0": 396.9, "1": 343.2},
+                "LSTAT": {"0": 4.98, "1": 5.1},
             }
         ),
         content_type="application/json",
@@ -34,6 +34,21 @@ def test_predict_invalid_json(test_app):
 
     # When
     resp = client.post("/predict", data=json.dumps({}), content_type="application/json")
+
+    # Then
+    data = json.loads(resp.data.decode())
+    assert resp.status_code == 400
+    assert "Input payload validation failed" in data["message"]
+
+
+def test_predict_invalid_json_keys(test_app):
+    # Given
+    client = test_app.test_client()
+
+    # When
+    resp = client.post(
+        "/predict", data=json.dumps({"CHAS": {"0": 0}}), content_type="application/json"
+    )
 
     # Then
     data = json.loads(resp.data.decode())
